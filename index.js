23c3828b74f4fc2c914d0444e5e624edd8d13490
onlykartik <karthik.rdyy@gmail.com>
2021-12-06 10:27:00 +0530

    modeified index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var number = 8
 var about  = "my name is karthik im going to get a job in infosis, cognizent, vipro,"
 console.log(number + about)
+consoel.log("hi now im learning git nad git hub ")
