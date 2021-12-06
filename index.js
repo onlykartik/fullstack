7c92b9807446c1869e4489ef60ce607bc0b708ee
Karthik <94488926+onlykartik@users.noreply.github.com>
2021-12-06 10:31:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var number = 8
 var about  = "my name is karthik im going to get a job in infosis, cognizent, vipro,"
 console.log(number + about)
 consoel.log("hi now im learning git nad git hub ")
+var number = 88
+var karthiik = "my name"
+var ramya = "my mother"
+console.log(karthik + ramya);
