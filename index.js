edeba659949482dc3cc290344afab0a694363070
onlykartik <karthik.rdyy@gmail.com>
2021-12-06 10:41:22 +0530

    again modified

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var number = 8
-var about  = "my name is karthik im going to get a job in infosis, cognizent, vipro,"
-console.log(number + about)
+
+var about  = "my name is karthik, cognizent, vipro,"
+console.log( about)
 consoel.log("hi now im learning git nad git hub ")
 var number = 88
-var karthiik = "my name"
+var karthik = "my name"
 var ramya = "my mother"
 console.log(karthik + ramya);
